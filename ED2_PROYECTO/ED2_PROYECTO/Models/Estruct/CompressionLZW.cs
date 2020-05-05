27089f8e1bf9b0d298287552170a03628c32528b
Rodrigo <Villacinda>
2020-05-05 13:41:22 -0600

    ADD recursos

---
+++ ED2_PROYECTO/ED2_PROYECTO/Models/Estruct/CompressionLZW.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ED2_PROYECTO.Models.Estruct
+{
+    public class CompressionLZW
+    {
+    }
+}
