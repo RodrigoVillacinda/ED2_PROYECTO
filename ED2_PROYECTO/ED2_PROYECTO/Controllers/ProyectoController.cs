bb4b01ba7980d96ee202f49a68c6a34cb2cd4354
AlexUwU <alexander.2014022@gmail.com>
2020-05-08 20:02:27 -0600

    Cambios controlador

--- ED2_PROYECTO/ED2_PROYECTO/Controllers/ProyectoController.cs
+++ ED2_PROYECTO/ED2_PROYECTO/Controllers/ProyectoController.cs
@@ -4,13 +4,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ED2_PROYECTO.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
     public class ProyectoController : ControllerBase
     {
+        public static IWebHostEnvironment _environment;
+
+        public ProyectoController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+
+        }
+        public class FileUploadAPI
+        {
+            public IFormFile files { get; set; }
+        }
+
+        [HttpPost("BstarTree/insert/Producto")]
+        public IEnumerable<Producto> PostDrinks([FromBody] Producto bebida)
+        {
+            ED2_PROYECTO.Models.Data.Data.Instance.arbolP.Add(Producto);
+            D2_LABFINAL.Models.Btree.Data.Instance.arbol.Insertar(D2_LABFINAL.Models.Btree.Data.Instance.bebidas);
+            return D2_LABFINAL.Models.Btree.Data.Instance.bebidas;
+        }
+
+
+
     }
 }
